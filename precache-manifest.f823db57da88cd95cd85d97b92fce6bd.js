e63ade462f8414723d6b5a0f871fb2bf0fa2f3cc
narwalabhi <anarwal500@gmail.com>
2020-08-30 16:23:27 +0530

    Updates

---
+++ precache-manifest.f823db57da88cd95cd85d97b92fce6bd.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cea842382cf2d1f4b2c1a1bbd65d251",
+    "url": "/abhishek-portfolio/index.html"
+  },
+  {
+    "revision": "83be0a8f47d58854da10",
+    "url": "/abhishek-portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "edc9663beaaa103fe564",
+    "url": "/abhishek-portfolio/static/css/main.1a6f2bdf.chunk.css"
+  },
+  {
+    "revision": "83be0a8f47d58854da10",
+    "url": "/abhishek-portfolio/static/js/2.d190ab19.chunk.js"
+  },
+  {
+    "revision": "254c01d1a3c7d8124adf4c35e46b76e3",
+    "url": "/abhishek-portfolio/static/js/2.d190ab19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edc9663beaaa103fe564",
+    "url": "/abhishek-portfolio/static/js/main.8880ea11.chunk.js"
+  },
+  {
+    "revision": "183565f446a04fcb7e4a",
+    "url": "/abhishek-portfolio/static/js/runtime-main.b5f79d66.js"
+  },
+  {
+    "revision": "c2957e1b182c77eafe1c52d7aeb7ca1e",
+    "url": "/abhishek-portfolio/static/media/about.c2957e1b.jpg"
+  },
+  {
+    "revision": "2af4cb88778a2ee1d482340fac95f634",
+    "url": "/abhishek-portfolio/static/media/bgimg.2af4cb88.jpg"
+  },
+  {
+    "revision": "9a4f6ed9b7e97d27d21b9f40871164f3",
+    "url": "/abhishek-portfolio/static/media/bookings.9a4f6ed9.jpg"
+  },
+  {
+    "revision": "47361c43637f784e8680168af77b4f7f",
+    "url": "/abhishek-portfolio/static/media/chat.47361c43.jpg"
+  },
+  {
+    "revision": "65336ae0335a7e1e431ed14ce38bcba4",
+    "url": "/abhishek-portfolio/static/media/dashboard.65336ae0.jpg"
+  },
+  {
+    "revision": "3e4c0aaefb58de8f4b1c0047ca0b944e",
+    "url": "/abhishek-portfolio/static/media/login.3e4c0aae.jpg"
+  },
+  {
+    "revision": "e8a64d7e4d87522d7c6d0fc9eec70366",
+    "url": "/abhishek-portfolio/static/media/main.e8a64d7e.jpg"
+  },
+  {
+    "revision": "c5d1197f4cc4c0002a56b2fd6226712b",
+    "url": "/abhishek-portfolio/static/media/number.c5d1197f.jpg"
+  },
+  {
+    "revision": "0bd475c28134ef3b07cf7b7b37edf4fc",
+    "url": "/abhishek-portfolio/static/media/post.0bd475c2.jpg"
+  },
+  {
+    "revision": "959a5e5797feb1bfaf58564566467562",
+    "url": "/abhishek-portfolio/static/media/posts.959a5e57.jpg"
+  },
+  {
+    "revision": "c26be4ff70e70f079ed01dc677d9e337",
+    "url": "/abhishek-portfolio/static/media/profile.c26be4ff.jpg"
+  },
+  {
+    "revision": "a0448b5ea5b76c8c0996e4bfd939ff07",
+    "url": "/abhishek-portfolio/static/media/search.a0448b5e.jpg"
+  },
+  {
+    "revision": "dc5ef11e1d48049828473769b26a4a0d",
+    "url": "/abhishek-portfolio/static/media/speed.dc5ef11e.jpg"
+  },
+  {
+    "revision": "482924be3e4f8dd1c2535a95f1d85e93",
+    "url": "/abhishek-portfolio/static/media/users.482924be.jpg"
+  }
+]);

3364e4e30f0f51b146656530cc5c80d958f04d93
narwalabhi <anarwal500@gmail.com>
2020-08-30 19:47:08 +0530

    resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,8 +4,8 @@ import './Resume.css'
 const Resume = () => {
   return (
     <div className="resume-div">
-      <img className="resume-img" alt="resume page 1" src="https://res.cloudinary.com/shawabhishek/image/upload/v1598779675/narwal-page-001_ugys87.jpg" />
-      <img className="resume-img" alt="resume page 2" src="https://res.cloudinary.com/shawabhishek/image/upload/v1598779684/narwal-page-002_goj4sf.jpg" />
+      <img className="resume-img" alt="resume page 1" src="https://res.cloudinary.com/dn13kq97f/image/upload/v1598796683/portfolioo/my-cv-page-001_yq5sts.jpg" />
+      <img className="resume-img" alt="resume page 2" src="https://res.cloudinary.com/dn13kq97f/image/upload/v1598796683/portfolioo/my-cv-page-002_aoaq7z.jpg" />
     </div>
   );
 };
